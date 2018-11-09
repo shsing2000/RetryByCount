60836151ce639c2a8f30c928abd8b04324e286a3
Sam Hsing <sam.hsing@gmail.com>
2018-11-08 20:03:48 -0800

    start sample script testing, hacked to work

--- sample-script-prcoessors/nifi-sample-processors/src/test/java/com/samhsing/nifi/processors/BasicScriptTest.java
+++ sample-script-prcoessors/nifi-sample-processors/src/test/java/com/samhsing/nifi/processors/BasicScriptTest.java
@@ -16,6 +16,9 @@
  */
 package com.samhsing.nifi.processors;
 
+import org.apache.nifi.processors.script.ExecuteScript;
+import org.apache.nifi.script.ScriptingComponentHelper;
+import org.apache.nifi.script.ScriptingComponentUtils;
 import org.apache.nifi.util.TestRunner;
 import org.apache.nifi.util.TestRunners;
 import org.junit.Before;
@@ -25,14 +28,23 @@ import org.junit.Test;
 public class BasicScriptTest {
 
     private TestRunner testRunner;
+    private ScriptingComponentHelper scHelper;
 
     @Before
     public void init() {
+        testRunner = TestRunners.newTestRunner(new ExecuteScript());
+        testRunner.setValidateExpressionUsage(false);
+        scHelper = new ScriptingComponentHelper();
+        scHelper.createResources();
     }
 
     @Test
     public void testProcessor() {
-
+        testRunner.setProperty(scHelper.SCRIPT_ENGINE, "ECMAScript");
+        testRunner.setProperty(ScriptingComponentUtils.SCRIPT_FILE, "target/classes/scripts/BasicScript.js");
+        testRunner.assertValid();
+        testRunner.enqueue(new byte[0]);
+        testRunner.run();
+        testRunner.assertAllFlowFilesTransferred("success", 1);
     }
-
 }
